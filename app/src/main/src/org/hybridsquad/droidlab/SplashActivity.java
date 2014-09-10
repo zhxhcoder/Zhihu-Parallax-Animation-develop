5403fc07e4f8f377c4c80b09650c6aa632c751f0
Michael <zhuxiaohui001@163.com>
2014-09-10 19:20:47 +0800

    test

--- app/src/main/src/org/hybridsquad/droidlab/SplashActivity.java
+++ app/src/main/src/org/hybridsquad/droidlab/SplashActivity.java
@@ -10,7 +10,6 @@ import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextSwitcher;
-
 import com.viewpagerindicator.CirclePageIndicator;
 
 public class SplashActivity extends ActionBarActivity {
